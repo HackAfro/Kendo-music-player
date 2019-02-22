841e303f8911810a4e0541ad6ec1bb1e0eebf36e
Richard Afro <richyafro@hotmail.com>
2019-02-22 19:39:19 +0100

    modules file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { SongListComponent } from './song-list/song-list.component';
+import { SongItemComponent } from './song-item/song-item.component';
+import { HomeComponent } from './home/home.component';
+import { AlbumComponent } from './album/album.component';
+import { AlbumListComponent } from './album-list/album-list.component';
+import { LayoutModule} from '@progress/kendo-angular-layout';
+import {HttpClientModule} from '@angular/common/http';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SongListComponent,
+    SongItemComponent,
+    HomeComponent,
+    AlbumComponent,
+    AlbumListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ButtonsModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
